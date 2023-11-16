d403843d49c7ae7d9dbd0b31909f2d0dac05b699
munendra-successive <munendra.kushwaha@successive.tech>
2023-11-16 20:38:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+var app = express();
+app.use(cookieParser());
+
+app.get("/", function (req, res) {
+  res.cookie('username','monu')
+  res.cookie('isYoung','true',{
+    maxAge:900000, httpOnly:true
+  })
+  res.send("Cookies example")
+  console.log("Cookies:",req.cookies);
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port: 8080");
+});
