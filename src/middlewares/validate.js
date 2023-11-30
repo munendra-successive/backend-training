409ee3bb3b9ffdc9dbd2e9f993a1d6bb15f5bc4c
munendra-successive <munendra.kushwaha@successive.tech>
2023-11-30 15:14:46 +0530

    validate all fields

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,13 +1,16 @@
 import { userConfig } from "../utils/config.js";
 const validate = (req, res, next) => {
-  console.log(req.originalUrl);
-  const mySchema = userConfig(req.originalUrl);
-  const { error, value } = mySchema.validate(req.body, { abortEarly: false });
-  if (error) {
+  try {
+    console.log(req.originalUrl);
+    const mySchema = userConfig(req.originalUrl);
+    const { error, value } = mySchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      throw error;
+    }
+    next();
+  } catch (error) {
     return res.status(400).json(error.details);
   }
-  console.log(value);
-  next();
 };
 
 export default validate;
